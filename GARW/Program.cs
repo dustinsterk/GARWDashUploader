224b210b30a7a25cf6e84957194fafa8d402452e
Dustin Sterkenburg <dustin.sterkenburg@gmail.com>
2024-01-03 20:39:49 -0500

    Added a --reboot option

--- GARW/Program.cs
+++ GARW/Program.cs
@@ -6,25 +6,68 @@ class Program
     {
         int numOfArgs = args.Length;
 
-        //Check for invalid args
+        //Check for zero args
         if (numOfArgs == 0)
         {
+            System.Console.WriteLine("Invalid parameters...please review the usage:");
             PrintUsage();
             return;
         }
 
-        //Check for invalid args
+        //Check for an invalid number of args (currently max of 2)
         if (numOfArgs > 2)
         {
+            System.Console.WriteLine("Invalid parameters...please review the usage:");
             PrintUsage();
             return;
         }
 
-        //Parse args
+        //Correct number of args passed, now parse the args for execution
         if (numOfArgs == 1)
         {
-            if (args[0] == "--purge")
+            //Reboot option
+            if (args[0] == "--reboot")
+            {     
+                System.Console.WriteLine("Reboot process initialized.");
+                //Connect to the GARW to reboot
+                    using (var client = new SshClient("192.168.42.1", "root", "root"))
+                    {
+                        string execCmd = "";
+                        try{
+                            client.Connect();
+                        }
+                        catch (Exception e){
+                            if (!client.IsConnected)
+                            {
+                                System.Console.WriteLine("No wifi connection, are you connected to the GARW SSID?");
+                                return;
+                            }
+                        }
+                        System.Console.WriteLine("Reboot the GARW now? (Y or N)");
+                        string confirmRemoval = Console.ReadLine();
+                        if (confirmRemoval.Length == 0)
+                        {
+                            System.Console.WriteLine("Operation aborted.");
+                            client.Disconnect();
+                        }
+                        if (confirmRemoval.ToUpper().Trim() == "Y"){
+                            System.Console.WriteLine("Rebooting the GARW system...");
+                            //reboot the GARW
+                            execCmd = "reboot now";
+                            client.RunCommand(execCmd);
+                            System.Console.WriteLine("GARW reboot initiated.");
+                        }
+                        else
+                        {
+                            System.Console.WriteLine("Operation aborted.");
+                            client.Disconnect();
+                        }
+                    }    
+            }  
+            //Purge option
+            else if (args[0] == "--purge")
             {
+                System.Console.WriteLine("Purge process initialized.");
                 System.Console.WriteLine("Are you sure you want to remove all 3rd party dash designs from your system? (Y or N)");
                 string confirmRemoval = Console.ReadLine();
                 if (confirmRemoval.Length == 0)
@@ -54,9 +97,11 @@ class Program
                         execCmd = "rm -rf /opt/Garw_IC7/library/";
                         client.RunCommand(execCmd);
                         System.Console.WriteLine("Resetting screen enabled file...");
-                        //fix the screen enabled file
+                        //fix the screen_enabled .txt file since if a new screen was set and now missing, it will cause a blank screen on reboot and empty browse previews in settings
+                        //remove the existing file
                         execCmd = "rm /opt/Garw_IC7/screen_enabled.txt";
                         client.RunCommand(execCmd);
+                        //create the file which needs 5 screens to be added, otherwise browse in settings will be empty and missing the preview pictures
                         execCmd = "echo -e '1\n2\n3\n4\n5' >> /opt/Garw_IC7/screen_enabled.txt";
                         client.RunCommand(execCmd);
                         System.Console.WriteLine("Rebooting the GARW system...");
@@ -74,24 +119,27 @@ class Program
             }
             else 
             {
+                System.Console.WriteLine("Invalid parameters...please review the usage:");
                 PrintUsage();
                 return;
             }
         }
 
-        //Parse args
+        //Input option execution
         if (numOfArgs == 2)
         {
             if (args[0] == "--input")
             {
+                System.Console.WriteLine("Input/Upload process initialized.");
                 string uploadZip = args[1];
                 string dashFileName = Path.GetFileName(uploadZip);
 
                 //Check the local path of the file to ensure it ia valid
                 if (File.Exists(uploadZip)){
                     string fileName = Path.GetFileNameWithoutExtension(uploadZip);
-                    //Strip the '_main' to check for the special case and to make the setting screen look cleaner
+                    //Strip the '_main' off of the filename and make the setting screen look cleaner
                     string dashName = fileName.Replace("_main","");
+                    //Path to be added into the config file
                     string dashPathToAdd = "file:///opt/Garw_IC7/library/" + fileName + "/" + fileName + ".qml";
 
                     //Check zip file contents for a .qml with the same zip file name and a .png for the settings screen
@@ -99,14 +147,15 @@ class Program
                         int totalEntries = archive.Entries.Count;
                         int checkEntries = 0;
                         foreach (ZipArchiveEntry e in archive.Entries){
+                            //Check for the .png file needed for the preview in settings
                             if (Path.GetFileName(e.FullName) == fileName + ".qml.png"){
                                 checkEntries++;
                             }
-                            
+                            //Check for the dash screen itself
                             if (Path.GetFileName(e.FullName) == fileName + ".qml"){
                                 checkEntries++;
                             }
-
+                            //If the .zip is named '_main', it also contains a settings screen (by pressing the up key on the GARW), so ensure the dash screen design file also exists
                             if (fileName.ToLower().Contains("_main"))
                             {
                                 if (Path.GetFileName(e.FullName) == dashName + ".qml"){
@@ -118,7 +167,7 @@ class Program
                         if (fileName.ToLower().Contains("_main"))
                         {
                             if (checkEntries != 3){
-                                System.Console.WriteLine("Invalid .zip file contents [settings file detected].  You must ensure the '_main'.qml matches the zip name (case sensitive), you also include the base .qml (with no '_main' included in the name), and you have the proper .qml.png file included!");
+                                System.Console.WriteLine("Invalid .zip file contents [multi file design detected].  You must ensure the '_main'.qml matches the zip name (case sensitive), you also included the base .qml (with no '_main' included in the name), and you have the proper .qml.png file included!");
                                 return;
                             }
                         }   
@@ -127,7 +176,6 @@ class Program
                             System.Console.WriteLine("Invalid .zip file contents, you must ensure the .qml matches the zip name (case sensitive) and you have the proper .qml.png file included!");
                             return;
                         }
-
                         System.Console.WriteLine("The dash .zip file is valid, connecting to the GARW via wifi.");
                     }
 
@@ -223,7 +271,7 @@ class Program
                         execCmd = "grep -E '" + dashPathToAdd + "' /opt/Garw_IC7/library/screen_locations.txt || echo '" + dashPathToAdd + "' >> /opt/Garw_IC7/library/screen_locations.txt";
                         //System.Console.WriteLine(execCmd);
                         client.RunCommand(execCmd);
-                        System.Console.WriteLine("Reboot the GARW now? (Y or N)");
+                        System.Console.WriteLine("Reboot the GARW now -- recommended after adding a new dash screen? (Y or N)");
                         string confirmRemoval = Console.ReadLine();
                         if (confirmRemoval.Length == 0)
                         {
@@ -248,6 +296,7 @@ class Program
             }
             else 
             {
+                System.Console.WriteLine("Invalid parameters...please review the usage:");
                 PrintUsage();
                 return;
             }
@@ -261,7 +310,8 @@ class Program
             System.Console.WriteLine("Usage:");
             System.Console.WriteLine("  GARWDashUploader [options]");
             System.Console.WriteLine("Options:");
-            System.Console.WriteLine("  --input         The path to the .zip file that contains the new dash design to be added to your GARW device.");
-            System.Console.WriteLine("  --purge         Removes ALL the 3rd party dash designs from your GARW system (default dash screens are not affected).");
+            System.Console.WriteLine("  --input <filepath to .zip>      The path to the .zip file that contains the new dash design to be added to your GARW device.");
+            System.Console.WriteLine("  --purge                         Removes ALL the 3rd party dash designs from your GARW system (default dash screens are not affected).");
+            System.Console.WriteLine("  --reboot                        Reboots the GARW device (similar to disconnecting the battery/power).");
     }
 }
